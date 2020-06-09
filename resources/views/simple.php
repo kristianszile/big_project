0062c3c3d313c72dafff96cc4263daf452bb6adf
kristianszile <kristians.zile@gmail.com>
2020-06-09 17:53:56 +0300

    Added user auth

---
+++ resources/views/simple.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head><meta charset="UTF-8">
+<title><?php echo $action; ?></title>
+</head>
+<body><p><?php echo $eventid; ?></p>
+  <p><?php echo $action; ?></p>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello/world',
+'HelloWorldController@hello');
+
+Route::get('event/{action}/{event_id?}',
+  function($action, $event_id = null) {
+    echo 'I want to ', $action , ' ';
+    if (!empty($event_id))
+    echo $event_id;
+});
+
+Route::get('/event/{action}/{event_id?}', function($action,
+$event_id = "default") {
+$data = array('action'=>$action,'eventid'=>$event_id);
+return view('simple', $data);
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
